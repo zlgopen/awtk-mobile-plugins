adf6da8740ae89890a07a21a6357c9c13affa4eb
xianjimli <xianjimli@gmail.com>
2020-07-07 18:27:57 +0800

    update build json

--- sync_assets.sh
+++ sync_assets.sh
@@ -2,6 +2,5 @@
 
 cp -rf ../awtk/demos/assets .
 cp -fv ../awtk/demos/assets.c demos
-cp -fv ../awtk/demos/assets_dark.inc demos
-cp -fv ../awtk/demos/assets_default.inc demos
+cp -fv ../awtk/demos/assets/*.inc assets
 
